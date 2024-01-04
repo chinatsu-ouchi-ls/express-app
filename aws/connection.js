968da693c2eef1f7b3d23b0d8707e85660e5ff9a
大内千夏 <chinatsu.ouchi@ysinc.co.jp>
2024-01-04 16:03:07 +0900

    API仮置き

--- aws/connection.js
+++ aws/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
+  host: process.env.RDS_HOST,
+  user: process.env.RDS_USER,
   password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DATABASE,
   port: process.env.RDS_PORT,
-  database: process.env.RDS_DB_NAME,
 })
 
 module.exports = { connection }
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 // 既存のユーザー取得のルート
 router.get('/', (req, res, next) => {
-  const sql = 'select * from users'
+  const sql = 'select * from USER'
   connection.query(sql, (err, result, fields) => {
     if (err) {
       next(err)
@@ -15,17 +15,60 @@ router.get('/', (req, res, next) => {
 })
 
 // 新しいユーザーを追加するルート
-router.post('/add', (req, res, next) => {
-  const { name } = req.body
-  const sql = 'INSERT INTO users (name) VALUES (?)'
+// router.post('/', (req, res, next) => {
+//   const { name } = req.body
+//   const sql = 'INSERT INTO USER (name) VALUES (?)'
 
-  connection.query(sql, [name], (err, result) => {
+//   connection.query(sql, [name], (err, result) => {
+//     if (err) {
+//       next(err)
+//       return
+//     }
+//     console.log(result)
+//     res.send({ message: 'User added successfully', id: result.insertId })
+//   })
+// })
+
+// ユーザー詳細の取得
+router.get('/:userId', (req, res, next) => {
+  const userId = req.params.userId
+  const sql = `
+    SELECT 
+      u.id, 
+      u.name, 
+      u.entering_company_at, 
+      d.id AS 'dept.id', 
+      d.name AS 'dept.name', 
+      j.id AS 'jobCategory.id', 
+      j.name AS 'jobCategory.name' 
+    FROM USER u 
+    LEFT JOIN DEPT d ON u.dept_id = d.id 
+    LEFT JOIN JOB_CATEGORY j ON u.job_category_id = j.id 
+    WHERE u.id = ?`
+
+  connection.query(sql, [userId], (err, result) => {
     if (err) {
       next(err)
       return
     }
-    console.log(result)
-    res.send({ message: 'User added successfully', id: result.insertId })
+    if (result.length === 0) {
+      res.status(404).send({ message: 'User not found' })
+    } else {
+      const user = {
+        id: result[0].id,
+        name: result[0].name,
+        dept: {
+          id: result[0]['dept.id'],
+          name: result[0]['dept.name'],
+        },
+        jobCategory: {
+          id: result[0]['jobCategory.id'],
+          name: result[0]['jobCategory.name'],
+        },
+        enteringCompanyAt: result[0].enteringCompanyAt,
+      }
+      res.send({ status: 200, user })
+    }
   })
 })
 
