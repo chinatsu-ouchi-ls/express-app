ca25d18fd838239c5988c3f59b100dc1ab1bef58
大内千夏 <chinatsu.ouchi@ysinc.co.jp>
2024-01-10 11:58:56 +0900

    ADD: ユーザー削除API

--- routes/user/users.js
+++ routes/user/users.js
@@ -5,6 +5,7 @@ const getUsers = require('./getUsers')
 const getUser = require('./getUser')
 const getUserMaterials = require('./getUserMaterials')
 const getUserMaterial = require('./getUserMaterial')
+const deleteUser = require('./deleteUser')
 
 // ユーザー一覧の取得
 router.get('/', getUsers)
@@ -17,5 +18,9 @@ router.get('/:userId/materials', getUserMaterials)
 
 // ユーザーIDと研修IDに基づいて、特定の研修の詳細情報を取得
 router.get('/:userId/materials/:materialId', getUserMaterial)
+
+// ユーザー削除
+router.delete('/:userId', deleteUser)
+
 
 module.exports = router
